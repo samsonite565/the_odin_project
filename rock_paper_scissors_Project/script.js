28efea931741d56ff30823056fa686fee5f1ef82
Samson Thomas <samthomas553@gmail.com>
2026-06-19 19:19:27 +0530

    Finished the program with the final loop.

--- rock_paper_scissors_Project/script.js
+++ rock_paper_scissors_Project/script.js
@@ -1,8 +1,7 @@
 const prompt = require("prompt-sync")();
-
-let computerScore = 0;
-let humanScore = 0;
 let usrInput = prompt("What is your guess? ");
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerguess() {
   let randomNumber = Math.floor(Math.random() * [4 - 1] + 1);
@@ -17,35 +16,52 @@ function getComputerguess() {
     console.log("Scissors");
     return "Scissors";
   }
+  //Function returns a string value
 }
-//
-function getHumanguess(guess) {
-  guess = guess.toLowerCase().replace(/\b\w/g, (s) => s.toUpperCase());
-  let acceptableAnswers = ["Rock", "Paper", "Scissors"];
-  //Should have a return value in number to compare with the computer guess
-  //Checking user input to see if he has put the correct answer
-  if (guess.length === 4) return  "Rock";
+
+function getHumanGuess(guess) {
+  guess.toLowerCase();
+  if (guess.length === 4) return "Rock";
   else if (guess.length === 5) return "Paper";
   else if (guess.length === 8) return "Scissors";
   else {
     console.log("Please enter a valid answer");
     return null;
   }
+  //Function returns a string value
 }
 
-// Need to make it so the when you make the final comparision it is number with number instead of numbwer with string
+//Compare if 2 strings are equal to each other.
+//Compare if 2 strings are against each other based on number of letters
+//Add to human or computer score
+//Compleate a total of 5 rounds
 function playRound(computerChoice, humanChoice) {
-  let computerGuessLength = computerChoice.length;
-  let humanGuessLength = humanChoice.length;
-
-  if (computerGuessLength > humanGuessLength) {
+  if (humanChoice.length === computerChoice.length) {
+    console.log(
+      `Human choice: ${humanChoice} and ${computerChoice} are the same`,
+    );
+  } else if (humanChoice.length > computerChoice.length) {
+    console.log(`Human wins this round`);
+    console.log(computerChoice.length);
+    console.log(humanChoice.length);
+    humanScore += 1;
+  } else if (computerChoice.length > humanChoice.length) {
+    console.log(`Computer wins this one`);
+    console.log(computerChoice);
     computerScore += 1;
-    console.log(computerScore);
-    console.log(humanScore);
-  };
-  
-  // Make your function’s humanguess paraumeter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
-  // Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
-  // Increment the humanScore or computerScore variable based on the round winner.
+  }
 }
-playRound(getComputerguess(),getHumanguess(usrInput));
+
+function playGame() {
+  console.log(`Computer Score: ${computerScore}`);
+  console.log(`Human Score: ${humanScore}`);
+
+  while (humanScore <= 5 && computerScore <= 5) {
+    usrInput = prompt("What is your guess now? ");
+    playRound(getComputerguess(), getHumanGuess(usrInput));
+    console.log(`Human score: ${humanScore}`);
+    console.log(`Computer score: ${computerScore}`);
+  }
+}
+
+playGame();
