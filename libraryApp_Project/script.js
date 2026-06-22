6951a0ac5e858d70aee0dc922f7100148846055c
Samson Thomas <samthomas553@gmail.com>
2026-06-22 21:42:08 +0530

    added the skeleton of all the files. Set the base for the HTML, CSS, and the JS files. Need to start working on the HTML and the CSS file for the design of the website then move towards the JS element. Trying to go for a  simple theme so the project will finish quick.

---
+++ libraryApp_Project/script.js
@@ -0,0 +1,18 @@
+// All the book objects should be stored in an array, constructer needed for the books
+// Add a function that takes takes arguments and creates a book with them, then stores that book into the array
+// The book object should have a unique ID which should be generated using 'crypto.randomUUID()'
+
+const myLibrary = [];
+
+function Book() {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.ID = crypto.randomUUID();
+}
+
+function addBookToLibrary() {
+// Add a function that takes takes arguments and creates a book with them, 
+// then stores that book into the array
+
+}
