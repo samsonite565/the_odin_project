1440d9196361d36798a927ecc20ac097e52df8b0
Samson Thomas <samthomas553@gmail.com>
2026-06-24 21:34:41 +0530

    Added the function that would take in arguments for title, author, and pages. Need to figure out how to incorate them into the DOM. Still having difficulties with clearing out the input aspect after getting the data from submit.

--- libraryApp_Project/script.js
+++ libraryApp_Project/script.js
@@ -1,18 +1,28 @@
-// All the book objects should be stored in an array, constructer needed for the books
-// Add a function that takes takes arguments and creates a book with them, then stores that book into the array
+// All the book objects/things should be stored in an array, constructer is needed for the books
+// Add a function that takes takes arguments and creates a book with them
 // The book object should have a unique ID which should be generated using 'crypto.randomUUID()'
-
+const bookTitle = document.getElementById("bookTitle");
+const bookAuthor = document.getElementById("bookAuthor");
+const pages = document.getElementById("pages");
+const submitBtn = document.getElementById("submitBtn");
 const myLibrary = [];
 
-function Book() {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.ID = crypto.randomUUID();
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.ID = crypto.randomUUID();
 }
 
-function addBookToLibrary() {
-// Add a function that takes takes arguments and creates a book with them, 
-// then stores that book into the array
-
+function addBookToLibrary(title, author, pages) {
+  // Add a function that takes takes arguments and creates a Book item/thing with them,
+  // then stores that book into the array (myLibrary)
+  const newBook = new Book(title, author, pages);
+  myLibrary.push(newBook);
 }
+
+//Add event listener to the submit button
+submitBtn.addEventListener("click", () => {
+  addBookToLibrary(bookTitle, bookAuthor, pages);
+  console.log(myLibrary);
+});
