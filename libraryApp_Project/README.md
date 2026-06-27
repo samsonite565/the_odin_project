f5e8455a40812bfa41c5d0fde29754827a34dff9
Samson Thomas <samthomas553@gmail.com>
2026-06-27 21:50:12 +0530

    fixed up the alignment of the page. Trying to get the functionality done. Have been able to hide and reveal the form when hitting the new book btn.

---
+++ libraryApp_Project/README.md
@@ -0,0 +1,5 @@
+#### Library App Project | The Odin Project
+## Author: Samson Thomas 
+## First Major Javascript project
+
+This is by far one of my hardest projects so far. 
--- libraryApp_Project/script.js
+++ libraryApp_Project/script.js
@@ -1,28 +1,43 @@
-// All the book objects/things should be stored in an array, constructer is needed for the books
-// Add a function that takes takes arguments and creates a book with them
-// The book object should have a unique ID which should be generated using 'crypto.randomUUID()'
+const newBookBtn = document.getElementById("newBookBtn")
 const bookTitle = document.getElementById("bookTitle");
 const bookAuthor = document.getElementById("bookAuthor");
 const pages = document.getElementById("pages");
+const formDisplay = document.querySelector(".bookInfoForm");
 const submitBtn = document.getElementById("submitBtn");
+const bookAdditionForm = document.getElementsByClassName("bookInfoForm")
 const myLibrary = [];
 
+// All the book objects/things should be stored in an array, constructer is needed for the books
+// The book object should have a unique ID which should be generated using 'crypto.randomUUID()'
 function Book(title, author, pages) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.ID = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.ID = crypto.randomUUID();
 }
 
+// Add a function that takes takes arguments and creates a book with them
 function addBookToLibrary(title, author, pages) {
-  // Add a function that takes takes arguments and creates a Book item/thing with them,
-  // then stores that book into the array (myLibrary)
-  const newBook = new Book(title, author, pages);
-  myLibrary.push(newBook);
+    // Add a function that takes takes arguments and creates a Book item/thing with them,
+    // then stores that book into the array (myLibrary)
+    const newBook = new Book(title, author, pages);
+    myLibrary.push(newBook);
 }
+
+//Reveal the form on "New Book Button"
+newBookBtn.addEventListener("click", () => {
+    const formStyleValue = formDisplay.style.getPropertyValue("display")
+    if (formStyleValue === "") {
+        formDisplay.style.display = "flex";
+    } else {
+        formDisplay.style.display = "";
+    }
+})
 
 //Add event listener to the submit button
 submitBtn.addEventListener("click", () => {
-  addBookToLibrary(bookTitle, bookAuthor, pages);
-  console.log(myLibrary);
+    addBookToLibrary(bookTitle.value, bookAuthor.value, pages.value); //Adds everthing to the array 
 });
+
+//Take the info from the array and add it to the card, Author, Title, pages etc.
+
