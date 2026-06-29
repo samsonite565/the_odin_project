4737b039769d5a0d6333108054c726dbec38e78b
Samson Thomas <samthomas553@gmail.com>
2026-06-29 21:40:17 +0530

    Finished everything. Not adding a background or much styling to the project as it was just a project to get an understanding about how constructers work.

--- libraryApp_Project/script.js
+++ libraryApp_Project/script.js
@@ -1,10 +1,10 @@
-const newBookBtn = document.getElementById("newBookBtn")
+const newBookBtn = document.getElementById("newBookBtn");
 const bookTitle = document.getElementById("bookTitle");
 const bookAuthor = document.getElementById("bookAuthor");
 const pages = document.getElementById("pages");
 const formDisplay = document.querySelector(".bookInfoForm");
 const submitBtn = document.getElementById("submitBtn");
-const bookAdditionForm = document.getElementsByClassName("bookInfoForm")
+const bookAdditionForm = document.getElementsByClassName("bookInfoForm");
 const myLibrary = [];
 
 // All the book objects/things should be stored in an array, constructer is needed for the books
@@ -18,7 +18,6 @@ function Book(title, author, pages) {
 
 // Add a function that takes takes arguments and creates a book with them
 function addBookToLibrary(title, author, pages) {
-    // Add a function that takes takes arguments and creates a Book item/thing with them,
     // then stores that book into the array (myLibrary)
     const newBook = new Book(title, author, pages);
     myLibrary.push(newBook);
@@ -26,18 +25,43 @@ function addBookToLibrary(title, author, pages) {
 
 //Reveal the form on "New Book Button"
 newBookBtn.addEventListener("click", () => {
-    const formStyleValue = formDisplay.style.getPropertyValue("display")
+    const formStyleValue = formDisplay.style.getPropertyValue("display");
     if (formStyleValue === "") {
         formDisplay.style.display = "flex";
     } else {
         formDisplay.style.display = "";
     }
-})
-
-//Add event listener to the submit button
-submitBtn.addEventListener("click", () => {
-    addBookToLibrary(bookTitle.value, bookAuthor.value, pages.value); //Adds everthing to the array 
 });
 
 //Take the info from the array and add it to the card, Author, Title, pages etc.
+function addInfoToCard(title, author, pages) {
+    // You want this function to take 3 arguments and 
+    // put the 3 arguments into the inner text of the card
+    const card = document.createElement("div"); // Creates a HTML element
+    card.classList.add("card", "align-center",  "bdr-radius", "pst-inherit"); // Method to choose all css properties
+
+    card.innerHTML = `
+        <h2>${title}</h2>
+        <p>${author}</p>
+        <p>${pages} pages</p>`
+
+    document.querySelector(".flex-row").appendChild(card)
+
+};
+
+//Add event listener to the submit button
+submitBtn.addEventListener("click", () => {
+    addBookToLibrary(
+        bookTitle.value,
+        bookAuthor.value,
+        pages.value
+    );
+    if (myLibrary.length > 0) {
+        addInfoToCard(
+            myLibrary[0].title,
+            myLibrary[0].author,
+            myLibrary[0].pages
+        );
+    }
+});
 
