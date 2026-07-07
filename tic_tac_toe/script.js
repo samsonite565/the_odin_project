f2d3d300568ec596a13ad0e632ec2a8109c2853f
Samson Thomas <samthomas553@gmail.com>
2026-07-07 23:41:19 +0530

    just the basic outline of the projects. Added the fonts and such. Need to work on the JS file and the functionality of the website before getting into the design

---
+++ tic_tac_toe/script.js
@@ -0,0 +1,5 @@
+// create a Tic tac to board
+// do not try to solve this similar to the last equation by using objects
+// It should keep count of the CPU score as well and the player's score
+// Total games played should be 5
+const playerScore = document.getElemenetsByClassName("player")
