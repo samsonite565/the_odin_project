1646a2a052ae52c3252c761a69c5f8896d58d6b8
Samson Thomas <samthomas553@gmail.com>
2026-07-17 21:35:50 +0530

    Deleted the old base reset css file and moved it into the new css base file. Started creating and modifiying the x and the o for the game. Added a new components file where the tic-tac toe board styles remain.

--- tic_tac_toe/script.js
+++ tic_tac_toe/script.js
@@ -8,12 +8,21 @@ function player() {
     let gamesPlayed = 0;
     const maxGames = 5;//Max games to be played is 5
 
+    /**
+     * gamesLeft.
+     */
     function gamesLeft() {
         return maxGames - gamesPlayed;
     };
+    /**
+     * addScore.
+     */
     function addScore() {
         score++;
     }
+    /**
+     * getScore.
+     */
     function getScore() {
         return score;
     }
@@ -31,14 +40,20 @@ function player() {
  * Game factory function. that creates a game object. 
  *It hs an Inheritence from the player object where.
  Create the board, checks for wins and resets the board.
- * @returns {[tic-toe-board, theWinner, startsGame, restetstheBoard]} 
+ * @returns {Function,Function,Function,Function} winCombos, playGame, checkWinner, resetBoard
  */
 function game() {
     // Creating the board Array
     const board = ["", "", "", "", "", "", "", "", ""]; //Board
     const gamePlayer = Object.create(player); //Inheritence 
+    /**
+     * getBoard.
+     */
     const getBoard = () => board;
 
+    /**
+     * winCombos.
+     */
     function winCombos() {
         let combos = []; //Combinations for wins
         for (let i = 0; i < 3; i++) {
@@ -50,12 +65,21 @@ function game() {
         return combos;
     }
 
+    /**
+     * playGame.
+     */
     function playGame() {
     }
     // Check the winning combos against a list of player's selections and if the list is correct return winner
+    /**
+     * checkWinner.
+     */
     function checkWinner() {
         return { winCombos, gamePlayer }
     }
+    /**
+     * resetBoard.
+     */
     function resetBoard() {
         return board.fill("");
     }
